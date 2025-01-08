c7865157c910ad2f1d41173ca69059424cab7d60
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-01-08 22:02:50 +0300

    Version 1.0, add more functionality, add main styling

--- index.js
+++ index.js
@@ -4,45 +4,55 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+// Main constants
 var app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const API_URL = "https://v2.jokeapi.dev/joke/"
 
+// Applying middlewares
 app.use(express.static(path.join(__dirname + "/public")));
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Main get request
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.post("/random-joke", async(req, res)=>{
+// Getting a random joke by posting an axious get request to the API
+app.get("/random-joke", async(req, res)=>{
     try{
         var content = await axios.get(API_URL + "Any");
         res.render("index.ejs", {jokes: content.data});
+    } catch(error){
+        console.error("Failed to make request: ", error.message);
+        res.render("index.ejs", {error: error.message});
     }
-    catch(error){
-        console.error("Failed to make request:", error.message);
-        res.render("index.ejs", {error: error.message,});
-    }
-
 });
 
+// Getting a specific joke by posting an axious get request with parameters to the API
 app.post("/query-joke", async(req, res)=>{
     var jokeCategory = req.body.jokeCategory;
     var jokeType = req.body.jokeType;
+    var jokeBlacklistFlags = req.body.blacklistFlags;
+
     try{
-        var content = await axios.get(API_URL + jokeCategory + "?type=" + jokeType);
+        var content = await axios.get(API_URL + jokeCategory, {
+            params: {
+                type: jokeType,
+                blacklistFlags: encodeURI(jokeBlacklistFlags),
+            }
+        });
         res.render("index.ejs", {queryJokes: content.data});
-    }
-    catch(error){
+    } catch(error){
         console.log("Failed to make request: ", error.message);
-        res.render("index.ejs", {error: error.message,});
+        res.render("index.ejs", {error: error.message});
     }
 });
 
+// Main port that connects to the server
 app.listen(port, ()=>{
     console.log(`Sever listening to port ${port}`);
 });
